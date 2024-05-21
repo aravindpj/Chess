69397d9faa9374a2cff4304ddade5e7c16ca2183
Aravind <aravind@aravind-MacBook-Air.local>
2024-05-21 22:04:24 +0530

    message sending issue resolved

--- backend/dist/Game.js
+++ backend/dist/Game.js
@@ -1,1 +1,63 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Game = void 0;
+const chess_js_1 = require("chess.js");
+const messages_1 = require("./messages");
+class Game {
+    constructor(playerOne, playerTwo) {
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+        this.board = new chess_js_1.Chess();
+        this.startTime = new Date();
+        this.moveCount = 0;
+    }
+    makeMove(socket, move) {
+        // when player make second move both condition check , and validate socket 
+        if (this.moveCount % 2 == 0 && socket !== this.playerOne) {
+            console.log(this.board.turn());
+            return;
+        }
+        if (this.moveCount % 2 == 1 && socket !== this.playerTwo) {
+            console.log(this.board.turn());
+            return;
+        }
+        try {
+            this.board.move(move);
+        }
+        catch (error) {
+            console.log(error);
+            return;
+        }
+        if (this.board.isGameOver()) {
+            this.playerOne.send(JSON.stringify({
+                type: messages_1.GAMEOVER,
+                payload: {
+                    winner: this.board.turn() === "w" ? "black" : "white",
+                },
+            }));
+            this.playerTwo.send(JSON.stringify({
+                type: messages_1.GAMEOVER,
+                payload: {
+                    winner: this.board.turn() === "w" ? "black" : "white",
+                },
+            }));
+        }
+        // is that first playersend message to second player
+        if (this.moveCount % 2 == 0) {
+            this.playerTwo.send(JSON.stringify({
+                type: "Move",
+                player: this.board.turn() === "w" ? "black" : "white",
+                payload: move
+            }));
+        }
+        else {
+            this.playerOne.send(JSON.stringify({
+                type: "Move",
+                player: this.board.turn() === "w" ? "black" : "white",
+                payload: move
+            }));
+        }
+        this.moveCount++;
+    }
+}
+exports.Game = Game;
--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,11 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
-//initilize websocket port 
+const GameManger_1 = __importDefault(require("./GameManger"));
+//initilize websocket port
 const wss = new ws_1.WebSocketServer({ port: 8080 });
+const gameManger = new GameManger_1.default();
 wss.on("connection", (ws) => {
-    ws.on("message", (data) => {
-        console.log(data.toString("utf-8"));
-    });
+    console.log("Log");
+    gameManger.addUser(ws);
     ws.send("Hai welcome to chess game");
 });
